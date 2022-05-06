63c4e60790b9f1f748c61437697bde60c90be24e
Sasi kumar <95676521+sasi-kumar-repo@users.noreply.github.com>
2022-05-06 12:03:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+YOUTUBE_BASE_URL = "https://www.youtube.com/"
+GOOGLE_LOGIN = "https://accounts.google.com/ServiceLogin"
+
+USER = ""
+PASSWORD = ""
+AUTHOR = ""
+
+SEARCH_CHANNEL_NAME = ""
+
+
+WORD_LIST = []
